7cea2d23e7e96e5fcbd56d22f8ea15669db5caa2
Matt Shearing <matt@adigital.co.uk>
2018-07-02 11:50:34 +0100

    refactored ajax around submissions to get file data working

--- src/assetbundles/zendeskwidgetwidget/dist/js/ZendeskWidget.js
+++ src/assetbundles/zendeskwidgetwidget/dist/js/ZendeskWidget.js
@@ -11,31 +11,24 @@
  */
 
 $(document).ready(function(){
-	$("#zendeskWidgetForm input[name='zendeskTicketSubmit']").click(function(e){
-		var formBody = $(this).parent().parent();
+	$("#zendeskWidgetForm").submit(function(e){
+		var formBody = $(this).parent();
 		var response;
-		var fields = {};
-		$("#zendeskWidgetForm input[type='text']").each(function(){
-			fields[$(this).attr("name")] = $(this).val();
-		});
-		$("#zendeskWidgetForm input[type='hidden']").each(function(){
-			fields[$(this).attr("name")] = $(this).val();
-		});
-		$("#zendeskWidgetForm select").each(function(){
-			fields[$(this).attr("name")] = $(this).val();
-		});
-		$("#zendeskWidgetForm textarea").each(function(){
-			fields[$(this).attr("name")] = $(this).val();
-		});
-		$("#zendeskWidgetForm input[type='file']").each(function(){
-			fields[$(this).attr("name")] = $(this).val();
-		});
-		$.post("/actions/zendesk/default/support-ticket", fields).done(function(data){
-			response = data;
-		}).fail(function(){
-			response = "<p>Error: We are sorry but your ticket was not submitted correctly.</p>";
-		}).always(function(){
-			formBody.html(response);
+		var submission = new FormData(this);
+		$.ajax({
+			url : "/actions/zendesk/default/support-ticket",
+			type: "POST",
+			data : submission,
+			processData: false,
+			contentType: false,
+			success:function(data, textStatus, jqXHR){
+				response = data;
+				formBody.html(response);
+			},
+			error: function(jqXHR, textStatus, errorThrown){
+				response = "<p>Error: We are sorry but your ticket was not submitted correctly.</p>";
+				formBody.html(response);
+			}
 		});
 		e.preventDefault();
 	});
